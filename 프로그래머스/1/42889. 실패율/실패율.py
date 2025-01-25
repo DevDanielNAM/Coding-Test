09d2e248637fc6940d71c50561eed71cf9c2735a
Kiseong NAM <108088887+DevDanielNAM@users.noreply.github.com>
2025-01-26 05:49:08 +0900

    [level 1] Title: 실패율, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,15 +1,17 @@
+from collections import Counter
+
 def solution(N, stages):
+    stage_counts = Counter(stages)
     n_person = len(stages)
     f_rate = []
-    
+
     for i in range(1, N + 1):
         if n_person > 0:
-            count = stages.count(i)
+            count = stage_counts[i]
             f_rate.append((i, count / n_person))
             n_person -= count
         else:
             f_rate.append((i, 0))
-    
-    answer = [stage for stage, rate in sorted(f_rate, key=lambda x: (-x[1], x[0]))]
-    
-    return answer
+
+    return [stage for stage, _ in sorted(f_rate, key=lambda x: (-x[1], x[0]))]
+
