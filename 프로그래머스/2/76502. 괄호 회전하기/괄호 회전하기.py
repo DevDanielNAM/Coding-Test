2e7b2ea03fc8663d84633d3bb1795b82667224a8
Kiseong NAM <108088887+DevDanielNAM@users.noreply.github.com>
2025-01-28 19:10:25 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,22 +1,21 @@
 def solution(s):
     answer = 0
-    
-    stack = []
-    p = {')':'(', '}':'{', ']':'['}
+    p = {')': '(', '}': '{', ']': '['}
+    n = len(s)
 
-    for i in range(len(s)):
-        stack.clear()
-        if i != 0:
-            s = s[1::] + s[0:1]
-
-        for char in s:
+    for i in range(n):
+        stack = []
+        rotated = s[i:] + s[:i]
+        
+        for char in rotated:
             if char in p.values():
                 stack.append(char)
-            elif stack and p[char] == stack[-1]:
+            elif stack and stack[-1] == p[char]:
                 stack.pop()
             else:
-                stack.append(-1)
                 break
-        if not stack: answer += 1
+        else:
+            if not stack:
+                answer += 1
 
     return answer
